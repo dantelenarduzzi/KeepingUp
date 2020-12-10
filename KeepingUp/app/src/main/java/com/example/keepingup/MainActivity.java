0e46a75cf12b53bcd3b123a89433b324fd63a3ca
dantelenarduzzi <56281257+dantelenarduzzi@users.noreply.github.com>
2020-12-09 21:59:18 -0500

    Updated calender view

--- KeepingUp/app/src/main/java/com/example/keepingup/MainActivity.java
+++ KeepingUp/app/src/main/java/com/example/keepingup/MainActivity.java
@@ -3,14 +3,12 @@ package com.example.keepingup;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
-import android.widget.CalendarView;
-import android.widget.TextView;
+
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,14 +18,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     private AppBarConfiguration mAppBarConfiguration;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
